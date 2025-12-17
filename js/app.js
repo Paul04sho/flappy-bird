4694ff1ff111548fb298db01d891791bf5a7e980
Paul- Emmanuel <paultchoume905@gmail.com>
2025-12-17 21:37:20 +0000

    Keyboard event listener inserted

--- js/app.js
+++ js/app.js
@@ -5,28 +5,34 @@ const ctx = canvas.getContext('2d');
 // Interface utilisateur
 const gameScreen = document.querySelector('.game-screen');
 const gameOverScreen = document.querySelector('.game-over-screen');
-const currentScoreDisplay= document.querySelector('current-score');
+const currentScoreDisplay= document.querySelector('.current-score');
 const scoreElement = document.getElementById('currentScore');
 const finalScore = document.getElementById('finalScore');
 const bestScore = document.getElementById('bestScore');
 const restartBtn = document.getElementById('restartButton');
 
-// Image de notre oiseau
-const bird = new Image();
-bird.src = "images/bird2.png";
-
-// Coordonnées de l'oiseau
-let bX = 10;
-let bY = 150;
-let gravity = 1.5;
-
-function drawBird() {
-    ctx.drawImage(bird, bX, bY, 40, 30);
-
-    bY += gravity;
-
-    requestAnimationFrame(drawBird);
+// Objets de jeu
+const bird = {
+    x: 50,
+    y: canvas.height / 2,
+    width: 34,
+    height: 24,
+    velocity: 0,
+    gravity: 0.6,
+    jumpStrength: -10,
+    color: 'yellow'
 };
 
-drawBird();
+let pipes = [];
+const pipeGap = 120;
+const pipeWidth = 60;
+const pipeSpeed = 2;
+
+document.body.addEventListener("keydown", (e) => {
+    console.log(`Touche pressée: ${e.key}`);
+})
+
+    
+
+
 
